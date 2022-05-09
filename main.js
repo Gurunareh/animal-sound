687b2426ae718edf5d98dd12a2bc0b55f9e1f851
Gurunareh <85010055+Gurunareh@users.noreply.github.com>
2022-05-09 18:56:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //https://teachablemachine.withgoogle.com/models/O7twguDAo/
+
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier = ml5 .soundclassifier('https://teachablemachine.withgoogle.com/models/O7twguDAo/modelReady');
+
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+    
+}
